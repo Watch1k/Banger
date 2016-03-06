049b94d76a6ea23ddcfaa662ab42766256557cef
Watch1k <host.sf@gmail.com>
2016-03-07 01:00:16 +0200

    initial commit

---
+++ src/js/common.js
@@ -0,0 +1,45 @@
+head.ready(function() {
+    // phoneInd - phone identifier
+
+    // // Clear placeholder
+    // 	(function() {
+    // 		$('input,textarea').focus(function(){
+    // 				$(this).data('placeholder',$(this).attr('placeholder'))
+    // 				$(this).attr('placeholder','');
+    // 		});
+    // 		$('input,textarea').blur(function(){
+    // 			$(this).attr('placeholder',$(this).data('placeholder'));
+    // 		});
+    // 	}());
+
+    // // ScrollTo
+    // 	$(function(){
+    // 	    $('.main-nav').onePageNav({
+    // 			filter: ':not(.external)',
+    // 			scrollThreshold: 0.25,
+    // 			scrollSpeed: 1200,
+    // 			easing: 'swing',
+    // 			scrollOffset: 38
+    // 		});
+    // 	});
+
+    // // js-inview
+    // 	$('.js-inview').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    // 		if (isInView) {
+    // 				if (visiblePartY == 'top') {
+    // 				// top part of element is visible
+    // 			} else if (visiblePartY == 'bottom') {
+    // 				// bottom part of element is visible
+    // 			} else {
+    // 				// whole part of element is visible
+    // 			}
+    // 		} else {
+    // 			// element has gone out of viewport
+    // 		}
+    // 	});
+
+    // // WOW animation
+    // 	new WOW().init();
+
+
+});
